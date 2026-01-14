8d7bc8510f805d5ec8e2dc7f16efc65768b3d13c
FX-88 <fedesan3121@gmail.com>
2026-01-14 09:08:13 +0100

    i3 adjustments

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Avvia polybar con log
-echo "---" | tee -a /tmp/polybar.log
+# echo "---" | tee -a /tmp/polybar.log
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar avviata..."
+# echo "Polybar avviata..."
