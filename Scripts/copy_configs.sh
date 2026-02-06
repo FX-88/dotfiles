5fa31ef80bc42d39120456544c0291629d3494b7
FX-88 <fedesan3121@gmail.com>
2026-02-06 15:55:51 +0100

    copying file "fix"

--- Scripts/copy_configs.sh
+++ Scripts/copy_configs.sh
@@ -7,15 +7,15 @@ echo "Copying config files (i3, picom, polybar) to ~/.config ..."
 
 mkdir -p "$HOME/.config/i3" "$HOME/.config/picom" "$HOME/.config/polybar"
 
-cp -f "${REPO_ROOT}/i3/config" "$HOME/.config/i3/config"
-cp -f "${REPO_ROOT}/i3/exec.conf" "$HOME/.config/i3/exec.conf"
-cp -f "${REPO_ROOT}/i3/keybinds.conf" "$HOME/.config/i3/keybinds.conf"
-cp -f "${REPO_ROOT}/i3/window_control.conf" "$HOME/.config/i3/window_control.conf"
+cp -rf "${REPO_ROOT}/i3/config" "$HOME/.config/i3/config"
+cp -rf "${REPO_ROOT}/i3/exec.conf" "$HOME/.config/i3/exec.conf"
+cp -rf "${REPO_ROOT}/i3/keybinds.conf" "$HOME/.config/i3/keybinds.conf"
+cp -rf "${REPO_ROOT}/i3/window_control.conf" "$HOME/.config/i3/window_control.conf"
 
-cp -f "${REPO_ROOT}/picom/picom.conf" "$HOME/.config/picom/picom.conf"
+cp -rf "${REPO_ROOT}/picom/picom.conf" "$HOME/.config/picom/picom.conf"
 
-cp -f "${REPO_ROOT}/polybar/config.ini" "$HOME/.config/polybar/config.ini"
-cp -f "${REPO_ROOT}/polybar/launch.sh" "$HOME/.config/polybar/launch.sh"
+cp -rf "${REPO_ROOT}/polybar/config.ini" "$HOME/.config/polybar/config.ini"
+cp -rf "${REPO_ROOT}/polybar/launch.sh" "$HOME/.config/polybar/launch.sh"
 chmod +x "$HOME/.config/polybar/launch.sh"
 
 echo "Done. Restart i3 or run 'i3-msg reload' to apply."
